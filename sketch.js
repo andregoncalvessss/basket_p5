b9ccd6be2416dc2ca122e650d4ec7f33c0228847
andregoncalvessss <andrefilipegoncalves@ipvc.pt>
2026-02-09 03:14:27 +0000

    Versão melhorada do reiniciar

--- sketch.js
+++ sketch.js
@@ -1,36 +1,32 @@
 let microfone;
 let bola;
 let cesto;
-let estadoJogo = 1; 
+let estadoJogo = 1;
 let pontos = 0;
-let tempoRestante = 30;
-let tempoInicial = 0;
-
 let imagemFundo;
 
+// MUDANÇA: Uma variável simples que guarda os segundos que faltam
+let tempoRestante = 30;
 
 function preload() {
-  imagemFundo = loadImage('fundo.png'); 
+  imagemFundo = loadImage('fundo.png');
 }
 
 function setup() {
   createCanvas(800, 600);
-  
-  // Inicializar o Microfone
+
   microfone = new p5.AudioIn();
   microfone.start();
-  
-  // Criar os objetos a partir das Classes
+
   bola = new Bola();
   cesto = new Cesto();
-  
+
   textSize(20);
 }
 
 function draw() {
   image(imagemFundo, 0, 0, width, height);
 
-  // Gestão de Ecrãs
   if (estadoJogo === 1) {
     jogar();
   } else if (estadoJogo === 2) {
@@ -38,23 +34,25 @@ function draw() {
   }
 }
 
-
 function jogar() {
   desenharInterface();
-  let tempoAtual = millis();
-  let segundosPassados = int((tempoAtual - tempoInicial) / 1000);
-  let tempoFaltam = 30 - segundosPassados;
-  
-  if (tempoFaltam <= 0) {
-    estadoJogo = 2; 
+
+  // LÓGICA DO TEMPO (A SOLUÇÃO)
+  // deltaTime conta o tempo entre cada frame. Dividimos por 1000 para ter segundos.
+  if (tempoRestante > 0) {
+    tempoRestante -= deltaTime / 1000;
+  } else {
+    // Se o tempo for menor que 0, acaba o jogo
+    tempoRestante = 0;
+    estadoJogo = 2;
   }
 
   cesto.mover();
   cesto.desenhar();
-  let volume = microfone.getLevel(); 
+
+  let volume = microfone.getLevel();
   bola.atualizar(mouseX, volume);
   bola.desenhar();
-  
 
   if (bola.verificarColisao(cesto)) {
     pontos++;
@@ -63,48 +61,62 @@ function jogar() {
 }
 
 function ecraRepetir() {
-  background(0, 0, 0, 150); 
-  
+  background(0, 0, 0, 150);
   fill(255);
   textAlign(CENTER);
+  
   textSize(40);
   text("FIM DE JOGO", width / 2, height / 2 - 50);
-  
+
   textSize(25);
   text("Pontuação Final: " + pontos, width / 2, height / 2);
   text("Clica no rato para Reiniciar", width / 2, height / 2 + 60);
 }
 
+// ESTA FUNÇÃO RESOLVE O TEU PROBLEMA
 function mousePressed() {
+  // O 'userStartAudio' é obrigatório no Chrome para o mic funcionar
+  userStartAudio();
+
   if (estadoJogo === 2) {
-    estadoJogo = 1;
+    // 1. Resetar Pontos
     pontos = 0;
+    
+    // 2. Resetar a Bola
     bola.resetar();
+    
+    // 3. FORÇAR O TEMPO PARA 30 SEGUNDOS
+    // Como estamos a definir o valor diretamente, ele não pode ser negativo
+    tempoRestante = 30; 
+    
+    // 4. Voltar ao jogo
+    estadoJogo = 1;
   }
 }
 
-
 function desenharInterface() {
-  fill(0); 
+  fill(0);
   noStroke();
   textAlign(RIGHT);
   textSize(16);
   text("ANDRÉ GONÇALVES", width - 20, 30);
   text("Nº 29892", width - 20, 50);
   text("ECGM", width - 20, 70);
-  
 
   textAlign(LEFT);
   textSize(24);
   text("BASKETBALL", 20, height - 20);
-  
 
   textAlign(CENTER);
-  text("Tempo: " + (30 - int((millis() - tempoInicial)/1000)), width/2, 30);
-  text("Pontos: " + pontos, width/2, 60);
+  
+  // Arredondamos o número para não mostrar casas decimais (ex: 29.999)
+  let tempoFormatado = int(tempoRestante);
+  
+  text("Tempo: " + tempoFormatado, width / 2, 30);
+  text("Pontos: " + pontos, width / 2, 60);
 }
 
-
+/* --- CLASSES (BOLA E CESTO) --- */
 
 class Bola {
   constructor() {
@@ -117,15 +129,13 @@ class Bola {
 
   atualizar(posicaoRatoX, volumeMic) {
     if (this.lancada == false) {
-  
       this.x = posicaoRatoX;
       this.y = height - 50;
-  
       if (volumeMic > 0.1) {
         this.lancada = true;
-        this.velocidadeY = -10; 
+        this.velocidadeY = -10;
       }
-    } else { 
+    } else {
       this.y += this.velocidadeY;
       if (this.y < 0) {
         this.resetar();
@@ -134,11 +144,11 @@ class Bola {
   }
 
   desenhar() {
-    fill(255, 100, 0); 
+    fill(255, 100, 0);
     stroke(0);
     circle(this.x, this.y, this.tamanho);
   }
-  
+
   resetar() {
     this.lancada = false;
     this.y = height - 50;
@@ -156,40 +166,38 @@ class Bola {
 class Cesto {
   constructor() {
     this.x = width / 2;
-    this.y = 160;       
-    this.largura = 120; 
-    this.altura = 90;   
+    this.y = 160;
     this.velocidade = 3;
   }
 
   mover() {
     this.x += this.velocidade;
-    if (this.x > width - 60 || this.x < 60) { 
-      this.velocidade *= -1; 
-      if(this.velocidade > 0) this.velocidade = random(2, 6);
+    if (this.x > width - 60 || this.x < 60) {
+      this.velocidade *= -1;
+      if (this.velocidade > 0) this.velocidade = random(2, 6);
       else this.velocidade = random(-6, -2);
     }
   }
 
   desenhar() {
     rectMode(CENTER);
-    fill(255); 
+    fill(255);
     stroke(0);
     strokeWeight(2);
-    rect(this.x, this.y, this.largura, this.altura);
+    rect(this.x, this.y, 120, 90);
     noFill();
     stroke(255, 0, 0);
     strokeWeight(3);
     rect(this.x, this.y + 15, 50, 40);
-    stroke(255); 
+    stroke(255);
     strokeWeight(1);
     line(this.x - 20, this.y + 40, this.x - 15, this.y + 75);
     line(this.x + 20, this.y + 40, this.x + 15, this.y + 75);
-    line(this.x - 7,  this.y + 40, this.x - 5,  this.y + 75);
-    line(this.x + 7,  this.y + 40, this.x + 5,  this.y + 75);
+    line(this.x - 7, this.y + 40, this.x - 5, this.y + 75);
+    line(this.x + 7, this.y + 40, this.x + 5, this.y + 75);
     line(this.x - 18, this.y + 50, this.x + 18, this.y + 50);
     line(this.x - 12, this.y + 65, this.x + 12, this.y + 65);
-    stroke(255, 100, 0); 
+    stroke(255, 100, 0);
     strokeWeight(5);
     line(this.x - 30, this.y + 40, this.x + 30, this.y + 40);
     strokeWeight(1);
